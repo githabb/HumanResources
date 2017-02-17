418e31e77a7d13f64ff948dde127b5b18d3509a7
yuriy <yurievch@gmail.com>
2017-02-17 13:08:32 +0200

    Добавил валидацию на форму Редактирования

--- HumanResources.Web/Models/EmployeeModel.cs
+++ HumanResources.Web/Models/EmployeeModel.cs
@@ -6,13 +6,33 @@ namespace HumanResources.Web.Models
     public class EmployeeModel
     {
         public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Город")]
         public int CityId { get; set; }
+
         public string CityName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Фамилия")]
         public string Surname { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Имя")]
         public string Name { get; set; }
+
+        [MaxLength(50)]
+        [Display(Name = "Отчество")]
         public string Patronymic { get; set; }
+
+        [MaxLength(200)]
+        [Display(Name = "Адрес")]
         public string Address { get; set; }
 
+        [Required]
+        [Display(Name = "Дата старта")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime StartDate { get; set; }
 
@@ -27,6 +47,8 @@ namespace HumanResources.Web.Models
 
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime? DateOfBirth { get; set; }
+
+        [Required]
         public string Position { get; set; }
 
 
