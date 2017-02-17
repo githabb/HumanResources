6885bc1d6c394c0e5ceb03f568921d8c2100054b
yuriy <yurievch@gmail.com>
2017-02-17 02:36:57 +0200

    Реализовал страницы редактирования и создания

--- HumanResources.Web/Controllers/HomeController.cs
+++ HumanResources.Web/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,35 +13,273 @@ namespace HumanResources.Web.Controllers
 {
     public class HomeController : Controller
     {
+        #region Drop Downs
+
+        private IList<NameItem> GetEmployeeList(int? exceptId)
+        {
+            IList<EmployeeModel> employees;
+            using (HumanResourceEntities db = new HumanResourceEntities())
+            {
+                employees = (from b in db.Employees
+                    where (exceptId == null || exceptId != b.Id) && b.EndDate == null // проверяем, что сотрудник не уволился
+                    select new EmployeeModel
+                    {
+                        Id = b.Id,
+                        Surname = b.Surname,
+                        Name = b.Name,
+                        Patronymic = b.Patronymic
+                    }).OrderBy(e => e.Surname).ThenBy(e => e.Name).ToList();
+            }
+
+            IList<NameItem> list = new List<NameItem>();
+            list.Insert(0, new NameItem() { Id = null, Name = string.Empty });
+            foreach (var m in employees)
+            {
+                list.Add(new NameItem() { Id = m.Id, Name = EmployeeModel.GetDisplayName(m.Surname, m.Name, m.Patronymic) });
+            }
+            return list;
+        }
+
+
+        private IList<NameItem> GetCityList()
+        {
+            IList<CityModel> cities;
+            using (HumanResourceEntities db = new HumanResourceEntities())
+            {
+                cities = (from b in db.Cities
+                    select new CityModel
+                    {
+                        Id = b.Id,
+                        Name = b.Name
+                    }).OrderBy(e => e.Name).ToList();
+            }
+
+            IList<NameItem> list = new List<NameItem>();
+            list.Insert(0, new NameItem() { Id = null, Name = string.Empty });
+            foreach (var m in cities)
+            {
+                list.Add(new NameItem() { Id = m.Id, Name = m.Name });
+            }
+            return list;
+        }
+
+        private void LoadDropDowns(int? id)
+        {
+            var employees = GetEmployeeList(id);
+            // передаем всех сотрудников в динамическое свойство EmployeeList в ViewBag
+            ViewBag.EmployeeList = new SelectList(employees, "Id", "Name");
+
+
+            var cities = GetCityList();
+
+            // передаем все города в динамическое свойство CityList в ViewBag
+            ViewBag.CityList = new SelectList(cities, "Id", "Name");
+        }
+
+        #endregion
+
         public ActionResult Index()
         {
+
             // получаем из бд все объекты Book
             using (HumanResourceEntities db = new HumanResourceEntities())
             {
                 var employees = (from b in db.Employees
-                                 select new EmployeeModel
-                                 {
-                                     Id = b.Id,
-                                     CityId = b.CityId,
-                                     Name = b.Name,
-                                     Surname = b.Surname,
-                                     Patronymic = b.Patronymic,
-                                     Address = b.Address,
-                                     StartDate = b.StartDate,
-                                     EndDate = b.EndDate,
-                                     MobilePhone = b.MobilePhone,
-                                     ManagerId = b.ManagerId,
-                                     ManagerName = b.ManagerId == null ? null : b.Employee2.Surname,
-                                     DateOfBirth = b.DateOfBirth,
-                                     Position = b.Position,
-                                     Phone = b.Phone
-                                 }).ToList();
+                    select new EmployeeModel
+                    {
+                        Id = b.Id,
+                        CityId = b.CityId,
+                        Name = b.Name,
+                        Surname = b.Surname,
+                        Patronymic = b.Patronymic,
+                        Address = b.Address,
+                        StartDate = b.StartDate,
+                        EndDate = b.EndDate,
+                        MobilePhone = b.MobilePhone,
+                        ManagerId = b.ManagerId,
+                        ManagerName = b.ManagerId == null ? null : b.Employee2.Surname,
+                        DateOfBirth = b.DateOfBirth,
+                        Position = b.Position,
+                        Phone = b.Phone
+                    }).ToList();
 
                 // передаем все объекты в динамическое свойство Books в ViewBag
                 ViewBag.Employees = employees;
+
             }
             // возвращаем представление
             return View();
         }
+
+        [HttpGet]
+        public ActionResult Display(int? id)
+        {
+            using (HumanResourceEntities db = new HumanResourceEntities())
+            {
+                var employee = (from e in db.Employees
+                    where e.Id == id
+                    select new EmployeeModel
+                    {
+                        Id = e.Id,
+                        Name = e.Name,
+                        Surname = e.Surname,
+                        Patronymic = e.Patronymic,
+                        CityId = e.CityId,
+                        CityName = e.City.Name,
+                        StartDate = e.StartDate,
+                        EndDate = e.EndDate,
+                        MobilePhone =e.MobilePhone,
+                        ManagerId = e.ManagerId,
+                        DateOfBirth = e.DateOfBirth,
+                        Position = e.Position,
+                        Phone = e.Phone,
+                        Address = e.Address,
+                        ManagerName = e.ManagerId == null ? null : e.Employee2.Surname,
+                    }).FirstOrDefault();
+
+                if(employee == null)
+                    return HttpNotFound();
+
+                return View(employee);
+            }
+
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            using (HumanResourceEntities db = new HumanResourceEntities())
+            {
+                LoadDropDowns(id);
+
+                if (id == null)
+                    return HttpNotFound();
+
+                var employee = (from e in db.Employees
+                                where e.Id == id
+                                select new EmployeeModel
+                                {
+                                    Id = e.Id,
+                                    Name = e.Name,
+                                    Surname = e.Surname,
+                                    Patronymic = e.Patronymic,
+                                    CityId = e.CityId,
+                                    StartDate = e.StartDate,
+                                    EndDate = e.EndDate,
+                                    Phone = e.Phone,
+                                    MobilePhone = e.MobilePhone,
+                                    ManagerId = e.ManagerId,
+                                    DateOfBirth = e.DateOfBirth,
+                                    Position = e.Position,
+                                    Address = e.Address,
+                                }).FirstOrDefault();
+
+                if (employee == null)
+                    return HttpNotFound();
+                
+                return View(employee);
+            }
+
+        }
+        
+
+        [HttpPost]
+        public ActionResult Edit(int? id, EmployeeModel employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                LoadDropDowns(id);
+                return View(employee);
+            }
+
+            Employee dbEmployee = new Employee()
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                Surname = employee.Surname,
+                Patronymic = employee.Patronymic,
+                CityId = employee.CityId,
+                StartDate = employee.StartDate,
+                EndDate = employee.EndDate,
+                MobilePhone = employee.MobilePhone,
+                DateOfBirth = employee.DateOfBirth,
+                Position = employee.Position,
+                Phone = employee.Phone,
+                Address = employee.Address,
+                ManagerId = employee.ManagerId
+            };
+            
+            using (HumanResourceEntities db = new HumanResourceEntities())
+            {
+                db.Entry(dbEmployee).State = EntityState.Modified;
+
+                db.SaveChanges();
+            }
+
+            // перенаправляем на главную страницу
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            LoadDropDowns(null);
+
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(EmployeeModel employee)
+        {
+            if (!ModelState.IsValid)
+                return Create();
+                
+            Employee dbEmployee = new Employee()
+            {
+                Name = employee.Name,
+                Surname = employee.Surname,
+                Patronymic = employee.Patronymic,
+                CityId = employee.CityId,
+                StartDate = employee.StartDate,
+                EndDate = employee.EndDate,
+                MobilePhone = employee.MobilePhone,
+                DateOfBirth = employee.DateOfBirth,
+                Position = employee.Position,
+                Phone = employee.Phone,
+                Address = employee.Address,
+                ManagerId = employee.ManagerId
+            };
+
+            using (HumanResourceEntities db = new HumanResourceEntities())
+            {
+                db.Employees.Add(dbEmployee);
+                db.SaveChanges();
+            }
+
+            // перенаправляем на главную страницу
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return HttpNotFound();
+
+            using (HumanResourceEntities db = new HumanResourceEntities())
+            {
+                Employee employee = new Employee { Id = id.Value };
+                db.Entry(employee).State = EntityState.Deleted;
+                db.SaveChanges();
+
+                // перенаправляем на главную страницу
+                return RedirectToAction("Index");
+            }
+        }
+
     }
 }
--- HumanResources.Web/Models/EmployeeModel.cs
+++ HumanResources.Web/Models/EmployeeModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HumanResources.Web.Models
 {
@@ -6,17 +7,25 @@ namespace HumanResources.Web.Models
     {
         public int Id { get; set; }
         public int CityId { get; set; }
+        public string CityName { get; set; }
         public string Surname { get; set; }
         public string Name { get; set; }
         public string Patronymic { get; set; }
         public string Address { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime StartDate { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime? EndDate { get; set; }
+
         public string MobilePhone { get; set; }
         public string Phone { get; set; }
         public int? ManagerId { get; set; }
 
         public string ManagerName { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime? DateOfBirth { get; set; }
         public string Position { get; set; }
 
